33518e11bfcd8de6098d57350b14a03436224b10
Period 6 <braylen.diec@talawanda.org>
2024-02-21 14:06:43 -0500

    206

---
+++ src/Task2.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        //Variables
+        Scanner in = new Scanner(System.in);
+        int numOne = 0;
+        int numTwo = 0;
+        int numThree = 0;
+        boolean done = false;
+        String trash;
+
+        //loop
+        do {
+
+            System.out.println("Enter the first number.");
+
+            if (in.hasNextInt()) {
+
+                numOne = in.nextInt();
+                System.out.println("Enter your second number.");
+
+                if (in.hasNextInt()) {
+
+                    numTwo = in.nextInt();
+                    System.out.println("What is the third number?");
+
+                    if (in.hasNextInt()) {
+
+                        numThree = in.nextInt();
+                        done = true;
+
+                    }
+
+                }
+
+            }
+
+            else {
+
+                trash = in.nextLine();
+                System.out.println("Your input was invalid: " + trash);
+
+            }
+
+        } while (!done);
+
+    }
+}
