85d069851dcc50d4475a11d054d75b4ee32e9026
Period 6 <braylen.diec@talawanda.org>
2024-02-22 14:06:00 -0500

    205

---
+++ src/Task3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        //Variables
+        Scanner in = new Scanner(System.in);
+        char userLetter = 0;
+        boolean done = false;
+
+        System.out.println("Enter a letter");
+
+
+
+        switch (userLetter) {
+
+            case:
+
+
+        }
+
+    }
+}
