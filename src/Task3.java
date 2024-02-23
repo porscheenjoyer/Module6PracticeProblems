5200cf1ebdc1f81274487cd028d7b97eff21e752
Period 6 <braylen.diec@talawanda.org>
2024-02-23 13:35:46 -0500

    135

--- src/Task3.java
+++ src/Task3.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Task3 {
@@ -5,19 +6,58 @@ public class Task3 {
 
         //Variables
         Scanner in = new Scanner(System.in);
-        char userLetter = 0;
+        String userLetter = " ";
         boolean done = false;
+        double number = 0;
 
-        System.out.println("Enter a letter");
+       do {
 
+           System.out.println("Enter a letter");
 
+           if (in.hasNextDouble()) {
 
-        switch (userLetter) {
+               number = in.nextDouble();
+               System.out.println("Need a letter, you gave : " + number);
+               in.nextLine();
 
-            case:
+           }
 
+           else {
 
-        }
+               userLetter = in.nextLine();
+               if (userLetter.length() == 1) {
+
+                   done = true;
+
+               }
+
+               else {
+
+                   System.out.println("Need a single letter");
+                   in.nextLine();
+
+               }
+
+           }
+
+       } while (!done);
+
+       userLetter = userLetter.toLowerCase();
+
+       switch (userLetter) {
+
+           case "a":
+           case "e":
+           case "i":
+           case "o":
+           case "u":
+               System.out.println("Your letter is a vowel");
+               break;
+           default:
+               System.out.println("Your letter is a constant");
+               break;
+
+       }
 
     }
 }
